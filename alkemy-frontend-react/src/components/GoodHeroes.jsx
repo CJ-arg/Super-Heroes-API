d51f84a4a9cd4148712421e6bd4a4c0b0ff9db83
unknown <janoncarlos@gmail.com>
2021-10-27 19:42:54 -0300

    home alt text todo login and validations

--- alkemy-frontend-react/src/components/GoodHeroes.jsx
+++ alkemy-frontend-react/src/components/GoodHeroes.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Card from './Card'
+import "./goodHeroes.css"
 import { useSelector, useDispatch } from 'react-redux'
 import { detailHeroeAction } from "../redux/actionReducers"
+import Search from './Search'
+import { Link } from 'react-router-dom'
 
 const GoodHeroes = () => {
   const goodHeroes = useSelector(store => store.searchList.goodOnes)
@@ -9,18 +12,33 @@ const GoodHeroes = () => {
   const dispatch = useDispatch();
   return (
     // <div className='container d-flex justify-content-center align-items-center gap-5 bg-info'>
-    <div className="container">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 " >
-
-        {
-          goodHeroes.map(card => (
-            <div className="mb-5" key={card.id}  ><Card title={card.name} id={card.id} url={card.image.url} gob={card.biography.alignment} powerstats={card.powerstats} /></div>
-          ))
+    <div className="container ">
 
 
-        }
+      {goodHeroes.length ?
+        goodHeroes.map(card => (<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 " >
 
-      </div></div>
+          <div className="mb-5" key={card.id}  ><Card title={card.name} id={card.id} url={card.image.url} gob={card.biography.alignment} powerstats={card.powerstats} /></div></div>
+
+        ))
+        :
+        <div className=" contText">
+
+
+          <div className=" noCardText">
+            <h2>It's time to recreate the ancient struggle of good against evil</h2> <p>There are a few RULES the team maximun number is 6 Heroes. The Good and the Bad side have 3
+              places each. Select your favourites  heroes using your mind your heart and also maths... </p>
+            <Link to='search'><button
+
+              className="btn btn btn-outline-light btn-lg goToSearch"
+
+            >Get a Team</button></Link> </div>
+        </div>
+
+
+      }
+
+    </div>
 
 
 
